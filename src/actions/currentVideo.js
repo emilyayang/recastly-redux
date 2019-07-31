7484fc5aa0450fab2e5d60f8c063953f4e8a9529
emilyayang <emilyayang@gmail.com>
2019-07-31 12:31:24 -0700

    Implementing actions

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,9 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+  return {
+    type: 'VIDEO_CHANGED',
+    currentVideo: video
+  }
 });
 
 export default changeVideo;
