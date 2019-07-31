6249e77bb83529096d73e00daa8b9f1dc9a7b383
emilyayang <emilyayang@gmail.com>
2019-07-31 15:49:29 -0700

    Finished actions

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,9 +1,7 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
-  return {
-    type: 'VIDEO_CHANGED',
-    currentVideo: video
-  }
+    type: 'CHANGE_VIDEO',
+    video: video
 });
 
 export default changeVideo;
